d21f043903eda1b7981e879657d9b2389dd2b0ab
General Zelgius <mattlow123@gmail.com>
2022-05-14 14:19:36 -0400

    started trying to add routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,7 +21,8 @@ const userController = {
         User.create(body)
         .then(dbUserData = res.json(dbUserData))
         .catch(err => res.status(400).json(err));
-    },
+    }
 
-    
-}
+};
+
+module.exports = userController;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { getAllUser, createUser } = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+module.exports = router;
